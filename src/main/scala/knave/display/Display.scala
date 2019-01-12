b59a6e9602ad5ae68359857c1930222754a95659
AddisonMink <addison.mink@gmail.com>
2019-01-12 17:10:11 -0600

    Refactored display.

--- src/main/scala/knave/display/Display.scala
+++ src/main/scala/knave/display/Display.scala
@@ -11,26 +11,39 @@ object Display {
   private val map = document.getElementById("map")
 
   private val tileArray = Array.ofDim[String](height,width)
+
   private def resetTileArray : Unit =
     for(y <- 0 until height)
       for(x <- 0 until width)
         tileArray(y)(x) = " "
   resetTileArray
 
+  private def buildString : String = {
+    val str = new StringBuilder
+    for(y <- 0 until height) {
+      for (x <- 0 until width)
+        str ++= tileArray(y)(x)
+      str += '\n'
+    }
+    str.toString
+  }
+
   private def show(symbol : String, color : String) : String =
     "<span style=\"color : " + color + "\">" + symbol + "</span>"
 
+  private def setTile(d : Dungeon, c : Coord) : Unit = {
+    lazy val floor = d.floorAt(c)
+    lazy val wall = d.wallAt(c)
+    lazy val door = d.doorAt(c)
+    if(floor.isDefined) tileArray(c.y)(c.x) = show(".", floor.get.color)
+    else if(wall.isDefined) tileArray(c.y)(c.x) = show("#", wall.get.color)
+    else if(door.isDefined) tileArray(c.y)(c.x) = if (door.get.open) show("/", door.get.color) else show("+", door.get.color)
+  }
+
   private def setDungeon(d : Dungeon) : Unit = {
     for(y <- 0 until height)
-      for(x <- 0 until width) {
-        val c = Coord(x,y)
-        lazy val floor = d.floorAt(c)
-        lazy val wall = d.wallAt(c)
-        lazy val door = d.doorAt(c)
-        if(floor.isDefined) tileArray(y)(x) = show(".", floor.get.color)
-        else if(wall.isDefined) tileArray(y)(x) = show("#", wall.get.color)
-        else if(door.isDefined) tileArray(y)(x) = if (door.get.open) show("/", door.get.color) else show("+", door.get.color)
-      }
+      for(x <- 0 until width)
+        setTile(d, Coord(x,y))
   }
 
   private def setPlayer(p : Player) : Unit =
@@ -40,12 +53,6 @@ object Display {
     resetTileArray
     setDungeon(w.dungeon)
     setPlayer(w.player)
-    val str = new StringBuilder
-    for(y <- 0 until height) {
-      for (x <- 0 until width)
-        str ++= tileArray(y)(x)
-      str += '\n'
-    }
-    map.innerHTML = str.toString
+    map.innerHTML = buildString
   }
 }
